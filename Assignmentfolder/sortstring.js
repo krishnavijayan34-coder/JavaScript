fe3d8741984ec88a2279d3e6457a81ed2aa2ff5f
krishna <krishnavijayan34@gmail.com>
2026-03-30 18:20:27 +0530

    sort string

---
+++ Assignmentfolder/sortstring.js
@@ -0,0 +1,8 @@
+const sortStrings = (arr) => {
+    return arr.sort();
+};
+
+let fruits = ["banana", "apple", "mango", "grape"];
+
+console.log("Original array:", fruits);
+console.log("Sorted array:", sortStrings(fruits));
