8cc7e7559eec0dbaafb58fddffa9512d95d45d5f
krishna <krishnavijayan34@gmail.com>
2026-03-31 11:24:44 +0530

    date get method examples

---
+++ getdatemethod.js
@@ -0,0 +1,13 @@
+let d = new Date();
+
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getFullYear()); 
+console.log(d.getHours());
+console.log(d.getMinutes());   
+console.log(d.getSeconds());    
+console.log(d.getMonth());     
+console.log(d.getTime()); 
+console.log(Date.now()); 
+console.log(d.getUTCFullYear());
+console.log(d.getUTCHours());
