bfe4aacb3708607d3d225ec7bcb194ce8b4ecbf5
krishna <krishnavijayan34@gmail.com>
2026-04-03 10:58:09 +0530

    class example

---
+++ class+object.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const person1 = new Person("John");
+const person2 = new Person("Jack");
+
+console.log(person1.name);
+console.log(person2.name);
