a883818536b328d2f9abf151a7657a6b635e69e8
krishna <krishnavijayan34@gmail.com>
2026-04-03 16:20:03 +0530

    comparison modified

--- assignmentfiles/compare2object.js
+++ assignmentfiles/compare2object.js
@@ -1,18 +1,26 @@
 function compareObjects(obj1, obj2) {
 
-    let keys = Object.keys(obj2);
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
 
-    for (let key of keys) {
+    
+    if (keys1.length !== keys2.length) {
+        return "Objects are not equal";
+    }
+
+    
+    for (let key of keys1) {
         if (obj1[key] !== obj2[key]) {
-            return false;
+            return "Objects are not equal";
         }
     }
 
-    return true;
+    return "Objects are equal";
 }
 
 
+
 const obj1 = { name: "krishna", age: 23, city: "Trivandrum" };
-const obj2 = { name: "krishna", age: 23 };
+const obj2 = { name: "krishna", age: 23, city: "Trivandrum" , qualification: "b.tech" };
 
 console.log(compareObjects(obj1, obj2));
