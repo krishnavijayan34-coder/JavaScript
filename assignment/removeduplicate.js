ccef3167a729a33d27e61d45d1e9cd136064d2dc
krishna <krishnavijayan34@gmail.com>
2026-04-03 16:50:29 +0530

    question 5

---
+++ assignment/removeduplicate.js
@@ -0,0 +1,5 @@
+const missedCalls = ["Vishnu","Akhil","Aswin","Pooja","Vishnu","Ananthan","Jith","Jith","Pooja","Aswin"];
+
+const uniqueCalls = [...new Set(missedCalls)];
+
+console.log(uniqueCalls);
